21da6e8eacad86aad410a1a425dc4d543a05dd26
Andrew Reed <andrew_reed@hotmail.com>
2021-03-25 14:08:00 +0000

    Added working command to read

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const core = require('@actions/core');
 const exec = require('child_process').exec;
 
 async function run() {
+    // const path = "test/test.properties";
     const path = core.getInput('path');
     console.log(`path:${path}`);
+    // const property = "version";
     const property = core.getInput('property');
     console.log(`property:${property}`);
-    exec(`$(sed -n "/^[[:space:]]*${property}[[:space:]]*=[[:space:]]*/s/^[[:space:]]*${property}[[:space:]]*=[[:space:]]*//p" "${path}")`, (error, stdout, stderr) => {
+    exec(`sed -n "/^[[:space:]]*${property}[[:space:]]*=[[:space:]]*/s/^[[:space:]]*${property}[[:space:]]*=[[:space:]]*//p" "${path}"`, (error, stdout, stderr) => {
         if(error != null) {
             core.setFailed(error);
         }
